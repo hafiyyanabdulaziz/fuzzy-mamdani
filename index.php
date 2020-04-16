dafe79efa618741ca9805b776faf43eb8cb85f86
Hafiyyan Abdul Aziz <hafiyyan.123@gmail.com>
2020-04-16 20:48:45 +0700

    Membuat input dan grafik suhu

--- index.php
+++ index.php
@@ -7,7 +7,82 @@
 </head>
 
 <body>
-
+    <form method="post" action="">
+        <table>
+            <tr>
+                <td>Suhu</td>
+                <td> : </td>
+                <td><input type="number" name="suhu" placeholder="Masukkan 0-100" required> Derajat Selsius</td>
+            </tr>
+            <tr>
+                <td>Kelembapan</td>
+                <td> : </td>
+                <td><input type="number" name="kelembapan" placeholder="Masukkan 0-100"> %</td>
+            </tr>
+            <tr>
+                <td>Tinggi Air</td>
+                <td>: </td>
+                <td><input type="number" name="tinggiair"> cm</td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="submit" value="Submit">
+                    <input type="reset" name="reset" value="Reset">
+                </td>
+            </tr>
+        </table>
+    </form>
 </body>
 
 </html>
+
+<?php
+//GRAFIK SUHU
+$nilaisuhuminimum = 0;
+$nilaisuhuoptimal = 0;
+$nilaisuhumaksimal = 0;
+if (isset($_POST["submit"])) {
+    echo "<h2>Solusi</h2>";
+    //suhu minimum
+    if ($_POST['suhu'] <= 5) {
+        $nilaisuhuminimum = 1;
+    } else {
+        if ($_POST['suhu'] < 15) {
+            $nilaisuhuminimum = (15 - $_POST['suhu']) / 10;
+        } else {
+            $nilaisuhuminimum = 0;
+        }
+    }
+    //suhu optimal
+    if ($_POST['suhu'] >= 15 && $_POST['suhu'] <= 35) {
+        if ($_POST['suhu'] >= 25 && $_POST['suhu'] <= 30) {
+            $nilaisuhuoptimal = 1;
+        } else {
+            if ($_POST['suhu'] >= 15 && $_POST['suhu'] < 25) {
+                $nilaisuhuoptimal = ($_POST['suhu'] - 15) / 10;
+            } else {
+                if ($_POST['suhu'] > 30 && $_POST['suhu'] < 35) {
+                    $nilaisuhuoptimal = (35 - $_POST['suhu']) / 5;
+                } else {
+                    $nilaisuhuoptimal = 0;
+                }
+            }
+        }
+    }
+    //suhu maksimal
+    if ($_POST['suhu'] >= 35) {
+        $nilaisuhumaksimal = 1;
+    } else {
+        if ($_POST['suhu'] >= 30 && $_POST['suhu'] < 35) {
+            $nilaisuhumaksimal = ($_POST['suhu'] - 30) / 5;
+        } else {
+            $nilaisuhumaksimal = 0;
+        }
+    }
+    echo $nilaisuhuminimum;
+    echo "<br>";
+    echo $nilaisuhuoptimal;
+    echo "<br>";
+    echo $nilaisuhumaksimal;
+}
+?>
